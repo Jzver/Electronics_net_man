14a8d33bcd66cb4c00a6703e2b441be10aa8fd11
Rf <jonnyzver78@gmail.com>
2024-12-30 11:03:46 +1000

    add migrations_user

--- config/users/admin.py
+++ config/users/admin.py
@@ -8,6 +8,6 @@ from users.models import User
 class UserAdmin(admin.ModelAdmin):
     """Класс для регистрации User в админке."""
 
-    list_display = ("id", "email", "is_active", "password", "first_name", "second_name")
+    list_display = ("id", "email", "is_active", "password", "first_name", "last_name")
     list_filter = ("email",)
     search_fields = ("email",)
